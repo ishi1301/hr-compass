ef185b875c9b026c1cafc167ba7fb722be4fe718
Ishita <ishhhhhita@gmail.com>
2026-06-09 12:02:18 +0530

    Use DATABASE_URL for production

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,23 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "hardik",
+//   host: "localhost",
+//   database: "irctc_hr",
+//   password: "",
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "hardik",
-  host: "localhost",
-  database: "irctc_hr",
-  password: "",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
